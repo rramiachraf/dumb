3e5edf2f162d1ad52c72986698b0657f2b595c64
rramiachraf <51409801+rramiachraf@users.noreply.github.com>
2023-06-05 17:37:29 +0100

    feat: try to bypass CF

--- utils.go
+++ utils.go
@@ -3,12 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
 	"net/url"
 	"path"
 	"text/template"
+	"time"
 
 	"github.com/allegro/bigcache/v3"
+	"github.com/caffix/cloudflare-roundtripper/cfrt"
 )
 
 var cache *bigcache.BigCache
@@ -80,11 +83,28 @@ func render(n string, w http.ResponseWriter, data interface{}) {
 
 const UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 
+var client = &http.Client{
+	Timeout: 20 * time.Second,
+	Transport: &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   15 * time.Second,
+			KeepAlive: 15 * time.Second,
+			DualStack: true,
+		}).DialContext,
+	},
+}
+
 func sendRequest(u string) (*http.Response, error) {
 	url, err := url.Parse(u)
 	if err != nil {
 		return nil, err
 	}
+
+	client.Transport, err = cfrt.New(client.Transport)
+	if err != nil {
+		return nil, err
+	}
+
 
 	req := &http.Request{
 		Method: http.MethodGet,
