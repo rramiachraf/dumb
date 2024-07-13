3e50e52d5e24a3a20739228c38190a624c407430
rramiachraf <51409801+rramiachraf@users.noreply.github.com>
2024-07-13 22:06:26 +0100

    chore: second attempt to sync branch with latest changes

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-<<<<<<< HEAD
-	"context"
-=======
 	"embed"
->>>>>>> upstream/main
 	"fmt"
 	"net"
 	"net/http"
@@ -22,36 +18,7 @@ import (
 var staticFiles embed.FS
 
 func main() {
-<<<<<<< HEAD
-	ctx := context.Background()
-	c, err := bigcache.New(ctx, bigcache.DefaultConfig(time.Hour*24))
-	if err != nil {
-		logger.Fatalln("can't initialize caching")
-	}
-	cache = c
-
-	r := mux.NewRouter()
-
-	r.Use(securityHeaders)
-
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { render("home", w, nil) })
-	r.HandleFunc("/search", searchHandler).Methods("GET")
-	r.HandleFunc("/{id}-lyrics", lyricsHandler)
-	r.HandleFunc("/{id}/{artist-song}/{verse}/annotations", annotationsHandler)
-	r.HandleFunc("/images/{filename}.{ext}", proxyHandler)
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	r.HandleFunc("/albums/{artist}/{albumName}", albumHandler)
-	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNotFound)
-		render("error", w, map[string]string{
-			"Status": "404",
-			"Error":  "page not found",
-		})
-
-	})
-=======
 	logger := utils.NewLogger(os.Stdout)
->>>>>>> upstream/main
 
 	server := &http.Server{
 		Handler:      handlers.New(logger, staticFiles),
